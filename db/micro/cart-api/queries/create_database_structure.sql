4c4bd9692add08f1dc4bb62520028e5772dc31c0
Júlio Borges <julioborges05@hotmail.com>
2024-06-16 18:49:35 -0300

    Adds the cart database scripts

---
+++ db/micro/cart-api/queries/create_database_structure.sql
@@ -0,0 +1,23 @@
+create sequence if not exists cart_sequence;
+
+create table if not exists cart(
+    id bigint not null default nextVal('cart_sequence'),
+    total_price float,
+    cart_status varchar(20) not null,
+    user_id bigint not null,
+    constraint cart_pkey primary key (id),
+    constraint user_id_fkey foreign key (user_id) references users (id) on delete cascade
+);
+
+
+create sequence if not exists cart_v_product_sequence;
+
+create table if not exists cart_v_product(
+    id bigint not null default nextVal('cart_v_product_sequence'),
+    cart_id bigint not null,
+    product_id bigint not null,
+    product_quantity integer not null default 1,
+    constraint cart_v_product_pkey primary key (id),
+    constraint cart_id_fkey foreign key (cart_id) references cart (id) on delete cascade,
+    constraint product_id_fkey foreign key (product_id) references product (id) on delete cascade
+);
